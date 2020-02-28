2977d0ac32446a84f4258b506b7f4a92586f0b19
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-02-28 00:19:05 -0300

    primeiro modulo funcional

--- GO/main.go
+++ GO/main.go
@@ -39,14 +39,14 @@ func main() {
 	abriu2 = scaner(IP2,porta2)
 
 	if abriu1 == true {
-		fmt.Printf("\nA primeira batida em: %s:%s retornou: OPEN\n",IP1,porta1)
+		fmt.Printf("\nA primeira batida em: %s:%d retornou: OPEN\n",IP1,porta1)
 	} else {
-		fmt.Printf("\nA primeira batida em: %s:%s retornou: CLOSED\n",IP1,porta1)
+		fmt.Printf("\nA primeira batida em: %s:%d retornou: CLOSED\n",IP1,porta1)
 	}
 
 	if abriu2 == true {
-		fmt.Printf("\nA Segunda batida em: %s:%s retornou: OPEN\n\n",IP2,porta2)
+		fmt.Printf("\nA Segunda batida em: %s:%d retornou: OPEN\n\n",IP2,porta2)
 	} else {
-		fmt.Printf("\nA Segunda batida em: %s:%s retornou: CLOSED\n\n",IP2,porta2)
+		fmt.Printf("\nA Segunda batida em: %s:%d retornou: CLOSED\n\n",IP2,porta2)
 	}
 }
