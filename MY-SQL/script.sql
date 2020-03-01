444d8076e026abd6f722ff48e8cc9d9c4289c632
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-02-29 23:29:34 -0300

    começando

--- MY-SQL/script.sql
+++ MY-SQL/script.sql
@@ -4,34 +4,63 @@ default collate utf8_general_ci;
 
 use pSgris;
 
-create table AVALIADORES (
+create table PALESTRANTES 
+(
 	id_palestrante					Tinyint(2) NOT NULL AUTO_INCREMENT,
-	palestrante							Varchar(50) NOT NULL,
+	nome										Varchar(50) NOT NULL,
 	aula										Varchar(20) NOT NULL,
+	avaliador								enum('S','N') NOT NULL default 'N', 
 	PRIMARY KEY (id_palestrante)
-)DEFAULT CHARSET = utf8;
+)
+ENGINE=innoDB
+AUTO_INCREMENT=1
+DEFAULT CHARSET = utf8;
 
-create table TAG (
+
+create table TAG 
+(
 	id_tag									Tinyint(2) NOT NULL AUTO_INCREMENT,
 	aula										Varchar(20) NOT NULL,
 	data_entrega						Date  NOT NULL,
+	CONSTRAINT fk_id_palestrante FOREIGN KEY (id_palestrante) REFERENCES PALESTRANTES (id_palestrante)
 	PRIMARY KEY (id_tag)
-)DEFAULT CHARSET = utf8;
+)
+ENGINE=innoDB
+AUTO_INCREMENT=1
+DEFAULT CHARSET = utf8;
 
-create table AVALIADOS (
-	id_avaliado							Tinyint(3) NOT NULL AUTO_INCREMENT,
-	nome										Varchar(50) NOT NULL,
+create table AVALIADOS 
+(
 	dre											Char(9) NOT NULL,
+	nome										Varchar(50) NOT NULL,
 	curso										Varchar(50) NOT NULL,
 	data_nascimento			  	Date NOT NULL,
 	periodo									TinyInt(2) NOT NULL,
-	PRIMARY KEY (id_avaliado)
-)DEFAULT CHARSET = utf8;
+	PRIMARY KEY (dre)
+)
+ENGINE=innoDB
+DEFAULT CHARSET = utf8;
 
-create table OUVINTES (
+create table NOTAS 
+(
+	id_notas								Tinyint(3) NOT NULL AUTO_INCREMENT,
+	CONSTRAINT fk_dre_avaliados FOREIGN KEY (dre) REFERENCES AVALIADOS (dre)
+	CONSTRAINT fk_id_tag FOREIGN KEY (id_tag) REFERENCES TAG (id_tag)
+	notas										Decimal(3,2) NOT NULL,
+	PRIMARY KEY (id_notas)
+)
+ENGINE=innoDB
+AUTO_INCREMENT=1
+DEFAULT CHARSET = utf8;
+
+create table OUVINTES 
+(
 	id_ouvinte							Tinyint(2) NOT NULL AUTO_INCREMENT,
 	nome										Varchar(50) NOT NULL,
 	intituicao_de_origem		Varchar(50),
 	ano_nascimento					Date NOT NULL,
 	PRIMARY KEY (id_ouvinte)
-)DEFAULT CHARSET = utf8;
+)
+ENGINE=innoDB
+AUTO_INCREMENT=1
+DEFAULT CHARSET = utf8;
