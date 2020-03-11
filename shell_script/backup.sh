5b5ba2d70fb7d8f8e4aa96358b0b6638e65217d7
Pedro-JGraca <pedrojullian@poli.ufrj.br>
2020-03-11 02:33:23 -0300

    0.0.2

--- shell_script/backup.sh
+++ shell_script/backup.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
 function VerificaUsuario() {
-#	USUARIOS=users
-#	echo ${USUARIOS[1]}
-	SAIDA=1
+
+	SAIDA=1 #1 usuario nao existe, 0 usuario existe
 }
 
 function EscolhaDoUsuario() {
-	declare - i i
+	declare -i i
+	declare -A USUARIO
 	i=0
 	SAIDA=1
 	P="P"
 	while [ $SAIDA -eq 1 ]; do
-	clear
-	echo "escolha um dos usuarios abaixo para fazer o backup" 
-	echo ""
-	sudo passwd -Sa | grep " P " >> a.txt
-	for line in $(cat a.txt);do 
-		if [[ ${line} =~ ^([0-9,/,-]+)$ ]];then
-			sleep 0s
-		else
-			if [ "$line" == "$P" ]; then
+		clear
+		echo "escolha um dos usuarios abaixo para fazer o backup" 
+		echo ""
+		sudo passwd -Sa | grep " P " >> a.txt
+		for line in $(cat a.txt);do 
+			if [[ ${line} =~ ^([0-9,/,-]+)$ ]];then
 				sleep 0s
 			else
-				echo $line
-				#USUARIO[$i]= $line
-				i=$[i+1]
-			fi
-		fi;done
-	sudo rm a.txt
-	echo $USUARIOS
-	echo ""
-	echo "Qual voce escolhe"
-	read DIR
-	VerificaUsuario
-	if [ $SAIDA -eq 1 ]; then
-		echo "esse usuario nao existe. Por favor escolha outro."
+				if [ "$line" == "$P" ]; then
+					sleep 0s
+				else
+					USUARIO[$i]=$line
+					echo ${USUARIO[$i]}
+					i=$[i+1]
+				fi
+			fi;done
+		sudo rm a.txt
 		echo ""
-		sleep 2s
-	else
-		echo "usuario $DIR escolhido" 
-	fi
+		echo "Qual voce escolhe"
+		read DIR
+		VerificaUsuario
+		if [ $SAIDA -eq 1 ]; then
+			echo "esse usuario nao existe. Por favor escolha outro."
+			echo ""
+			sleep 2s
+		else
+			echo "usuario $DIR escolhido" 
+		fi
 	done
 }
 
